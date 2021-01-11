1b4e578ca9b8a53277d0d9cd642b4e18df7cca5e
michaszo18 <michaszo18@gmail.com>
2021-01-11 17:36:37 +0100

    Initial commit

---
+++ notes/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
