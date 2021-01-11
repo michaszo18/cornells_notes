6fbf116979b2a3d17fa36f471b75c41622535932
michaszo18 <michaszo18@gmail.com>
2021-01-11 18:31:49 +0100

    added bootstrap

--- notes/models.py
+++ notes/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Note(models.Model):
     title = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, default="")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
